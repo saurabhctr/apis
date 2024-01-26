41a2c5705262aaeaf58aa6593cbe86c2b94fa22d
saurabhctr <157628585+saurabhctr@users.noreply.github.com>
2024-01-26 21:38:44 +0530

    Update with ec2 ready dumAPI.py

--- dumAPI.py
+++ dumAPI.py
@@ -68,4 +68,5 @@ def reverse():
     return jsonify({'result': reversed_data})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
